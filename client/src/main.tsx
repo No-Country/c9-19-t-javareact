fef1ce3841edce4f86c8c836a27672e0ff9f3c3a
fernandobouchet <fernandobouchet@gmail.com>
2023-02-12 16:02:42 -0300

    Added redux-toolkit base structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
