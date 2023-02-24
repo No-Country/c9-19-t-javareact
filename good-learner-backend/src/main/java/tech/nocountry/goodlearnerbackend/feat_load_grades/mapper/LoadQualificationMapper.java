b1f3cfa2910e38dd84edc94ec2ea1dd6c61d0be0
osirisdavid <davidosirisayala@gmail.com>
2023-02-23 23:05:03 -0300

    refactor by compilation

--- good-learner-backend/src/main/java/tech/nocountry/goodlearnerbackend/feat_load_grades/mapper/LoadQualificationMapper.java
+++ good-learner-backend/src/main/java/tech/nocountry/goodlearnerbackend/feat_load_grades/mapper/LoadQualificationMapper.java
@@ -1,13 +1,15 @@
 package tech.nocountry.goodlearnerbackend.feat_load_grades.mapper;
 
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import tech.nocountry.goodlearnerbackend.feat_load_grades.dto.LoadQualificationDTO;
 import tech.nocountry.goodlearnerbackend.model.Qualification;
 
 import java.util.List;
 import java.util.Optional;
 
-@Mapper
+//@Mapper
+@Component
 public class LoadQualificationMapper {
     public Optional<LoadQualificationDTO> mapToLoadQualificationDTO(Qualification qualification) {
         if (qualification == null) {
--- good-learner-backend/src/main/java/tech/nocountry/goodlearnerbackend/feat_show_commission/domain/mapper/CommissionMapper.java
+++ good-learner-backend/src/main/java/tech/nocountry/goodlearnerbackend/feat_show_commission/domain/mapper/CommissionMapper.java
@@ -2,12 +2,14 @@ package tech.nocountry.goodlearnerbackend.feat_show_commission.domain.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 import tech.nocountry.goodlearnerbackend.feat_show_commission.domain.dto.CommissionDTO;
 import tech.nocountry.goodlearnerbackend.model.Commission;
 
-@Mapper
+//@Mapper
+@Component
 public class CommissionMapper {
-    CommissionMapper INSTANCE = Mappers.getMapper( CommissionMapper.class );
+    //CommissionMapper INSTANCE = Mappers.getMapper( CommissionMapper.class );
 
     public CommissionDTO commissionToCommissionDTO(Commission commission) {
         return null;
