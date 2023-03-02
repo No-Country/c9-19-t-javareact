35366503c6865a89cc05c8394481fc8a8c1be28a
agustin <agupalavecino97@gmail.com>
2023-03-02 14:34:51 -0300

    conectar el asignar una nota a un estudiante con api

--- client/src/pages/MyCommissions.tsx
+++ client/src/pages/MyCommissions.tsx
@@ -86,33 +86,55 @@ function MyCommissions() {
         setShowSubjets(true);
     }
 
-    const handleSaveNewQualification = (i: number, period: number, newQualification: number) => {
+    const handleSaveNewQualification = async (i: number, period: number, newQualification: number, idStudent: number) => {
+        let periodName = '';
+        let method = '';
+        setLoading(true);
         switch(period) {
             case 1:
+                periodName = 'FIRST_TRIMESTER';
                 if (qualifications[i].qualifications!.FIRST_TRIMESTER === undefined) {
-
+                    method = 'POST';
                 } else {
-
+                    method = 'PUT';
                 }
                 qualifications[i].qualifications!.FIRST_TRIMESTER = Number(newQualification);
                 break;
             case 2:
+                periodName = 'SECOND_TRIMESTER';
                 if (qualifications[i].qualifications!.SECOND_TRIMESTER === undefined) {
-
+                    method = 'POST';
                 } else {
-
+                    method = 'PUT';
                 }
                 qualifications[i].qualifications!.SECOND_TRIMESTER = Number(newQualification);
                 break;
             case 3:
+                periodName = 'THIRD_TRIMESTER';
                 if (qualifications[i].qualifications!.THIRD_TRIMESTER === undefined) {
-
+                    method = 'POST';
                 } else {
-
+                    method = 'PUT';
                 }
                 qualifications[i].qualifications!.THIRD_TRIMESTER = Number(newQualification);
                 break;                         
         }
+        const apiPropertyes: apiProps = {
+            path: `teacher/qualification`,
+            method: method,
+            body:  {
+                "idStudent": idStudent,
+                "idCommissionSubject": selectedSubject.commissionSubjectId,
+                "periodName": periodName,
+                "numericalQualification": newQualification
+              }
+        }
+        const response = await useApi(apiPropertyes);
+        console.log(response);
+        if (response.data) {
+           
+        }
+        setLoading(false);
         setQualifications(qualifications);
         setShowModal(false);
     } 
