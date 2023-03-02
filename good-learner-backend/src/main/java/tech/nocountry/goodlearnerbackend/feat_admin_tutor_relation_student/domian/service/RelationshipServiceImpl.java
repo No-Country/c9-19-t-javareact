73e8754e7cbd822a2fc68d153788434c5bdcd66d
osirisdavid <davidosirisayala@gmail.com>
2023-03-02 18:24:48 -0300

    refactor realtionship student with tutor

--- good-learner-backend/src/main/java/tech/nocountry/goodlearnerbackend/feat_admin_tutor_relation_student/domian/service/RelationshipServiceImpl.java
+++ good-learner-backend/src/main/java/tech/nocountry/goodlearnerbackend/feat_admin_tutor_relation_student/domian/service/RelationshipServiceImpl.java
@@ -88,7 +88,7 @@ public class RelationshipServiceImpl implements IRelationshipService{
 
     @Override
     public ResponseEntity<?> createdRelation(RelationStudentTutorRequest relationStudentTutorRequest) {
-        RelationTutorStudentResponse relationStudentTutor = null;
+        DetailRelationTutorResponse relationStudentTutor = null;
 
         Optional<Student> student = studentRepository.findById(relationStudentTutorRequest.getIdStudent());
         Optional<Tutor> tutor = tutorRepository.findById(relationStudentTutorRequest.getIdTutor());
@@ -103,8 +103,11 @@ public class RelationshipServiceImpl implements IRelationshipService{
 
             TutorStudent tutorStudent = tutorStudentRepository.save(new TutorStudent(student.get(), tutor.get(), bond.get()));
 
-            relationStudentTutor = new RelationTutorStudentResponse(
+            relationStudentTutor = new DetailRelationTutorResponse(
+                    tutorStudent.getIdTutorStudent(),
+                    tutorStudent.getStudent().getIdPerson(),
                     tutorStudent.getStudent().getFirstName() + " " + tutorStudent.getStudent().getLastName(),
+                    tutorStudent.getTutor().getIdPerson(),
                     tutorStudent.getTutor().getFirstName() + " " + tutorStudent.getTutor().getLastName(),
                     tutorStudent.getBond().getBondName()
                     );
